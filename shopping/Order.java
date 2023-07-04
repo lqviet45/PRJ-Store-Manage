4ea6daf527987167a8de5521cb3e67ce7cb932b5
lqviet45 <112755108+lqviet45@users.noreply.github.com>
2023-07-04 16:04:44 +0700

    Add files via upload

---
+++ shopping/Order.java
@@ -0,0 +1,58 @@
+package sample.shopping;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author DELL
+ */
+public class Order {
+    private int orderID;
+    private String userID;
+    private Date date;
+    private double total;
+
+    public Order() {
+    }
+
+    public Order(int orderID, String userID, Date date, double total) {
+        this.orderID = orderID;
+        this.userID = userID;
+        this.date = date;
+        this.total = total;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    
+    
+}
---
+++ shopping/Tea.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.shopping;
+
+/**
+ *
+ * @author DELL
+ */
+public class Tea {
+
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String img;
+
+    public Tea() {
+    }
+
+    public Tea(String id, String name, double price, int quantity, String img) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.img = img;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
